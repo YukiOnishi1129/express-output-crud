669f6057d813baba3c22b316aed026a09ad660dc
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-10-08 16:52:46 +0900

    dotenv

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
+import * as dotenv from 'dotenv'
 import express, { Request, Response } from 'express';
 
+dotenv.config()
+
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
