4401260356f75cf228671167382a7c85761c0948
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-10-10 17:53:01 +0900

    migration env

--- backend/src/config/appDataSource.ts
+++ backend/src/config/appDataSource.ts
@@ -11,7 +11,7 @@ export const AppDataSource = {
       instance = new DataSource({
         type: 'mysql',
         host: process.env.MYSQL_CONTAINER_NAME,
-        port: Number(process.env.MYSQL_CONTAINER_PORT) || 3306,
+        port: Number(process.env.MYSQL_CONTAINER_PORT) || undefined,
         username: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE,
--- backend/src/config/migrationDataSource.ts
+++ backend/src/config/migrationDataSource.ts
@@ -5,11 +5,11 @@ dotenv.config();
 
 export const MigrationDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'user',
-  password: 'pass',
-  database: 'EXPRESS_OUTPUT_CRUD_DB',
+  host: process.env.MYSQL_HOST_USING_MIGRATION,
+  port: Number(process.env.MYSQL_PORT_USING_MIGRATION) || undefined,
+  username: process.env.MYSQL_USER_USING_MIGRATION,
+  password: process.env.MYSQL_PASSWORD_USING_MIGRATION,
+  database: process.env.MYSQL_DATABASE_USING_MIGRATION,
   entities: ['src/domain/entity/*.ts'],
   migrations: ['src/database/migrations/**/*.ts'],
   logging: true,
