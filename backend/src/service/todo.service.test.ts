720fe03d2dcf445d948cd46503bd4be53e2a5475
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-10-11 14:29:21 +0900

    【getTodoList】 service test

--- backend/src/service/todo.service.test.ts
+++ backend/src/service/todo.service.test.ts
@@ -3,15 +3,78 @@ import { Todo } from '@/domain/entity/todo.entity';
 
 import { getTodoList } from '@/service/todo.service';
 
-describe('Test todo.service ', () => {
+describe('【Service Test Todo】 ', () => {
   afterEach(async () => {
     const todoRepo = AppDataSource.getInstance().getRepository(Todo);
     await todoRepo.clear();
   });
-  describe('getTodoList', () => {
-    it('should return an empty todo list on success', async () => {
+  describe('【getTodoList】', () => {
+    it('Success: get 0 data', async () => {
       const todoList = await getTodoList({});
       expect(todoList).toEqual([]);
+    });
+    it('Success: get data', async () => {
+      const todoRepo = AppDataSource.getInstance().getRepository(Todo);
+      await todoRepo.save({
+        id: 1,
+        title: 'Test Todo1',
+        content: 'This is a test todo item1.',
+      });
+      await todoRepo.save({
+        id: 2,
+        title: 'Test Todo2',
+        content: 'This is a test todo item2.',
+      });
+
+      const todoList = await getTodoList({});
+      expect(todoList).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            id: 1,
+            title: 'Test Todo1',
+            content: 'This is a test todo item1.',
+          }),
+          expect.objectContaining({
+            id: 2,
+            title: 'Test Todo2',
+            content: 'This is a test todo item2.',
+          }),
+        ]),
+      );
+    });
+    it('Success: get data with keyword', async () => {
+      const todoRepo = AppDataSource.getInstance().getRepository(Todo);
+      await todoRepo.save({
+        id: 1,
+        title: 'Test Todo',
+        content: 'This is a test todo item.',
+      });
+      await todoRepo.save({
+        id: 2,
+        title: 'eest Todo2',
+        content: 'This is a test todo item2.',
+      });
+      await todoRepo.save({
+        id: 3,
+        title: 'Test Todo3',
+        content: 'This is a test todo item3.',
+      });
+
+      const todoList = await getTodoList({ keyword: 'Test' });
+      expect(todoList).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            id: 1,
+            title: 'Test Todo',
+            content: 'This is a test todo item.',
+          }),
+          expect.objectContaining({
+            id: 3,
+            title: 'Test Todo3',
+            content: 'This is a test todo item3.',
+          }),
+        ]),
+      );
     });
   });
 });
